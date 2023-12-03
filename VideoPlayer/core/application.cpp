fc6c4364a4e4341c0e16da111c1e03008f3cda3f
sc22my <sc22my@gmail.com>
2023-12-03 19:33:27 +0000

    correct wrong paths

--- VideoPlayer/core/application.cpp
+++ VideoPlayer/core/application.cpp
@@ -6,7 +6,7 @@ Application::Application(int argc, char* argv[]) :
     // If no args given, default assets path to cwd/assets
     if (argc < 2)
     {
-        m_Core = new Core({""});
+        m_Core = new Core({"."});
     } else
     {
         m_Core = new Core({argv[1]});
