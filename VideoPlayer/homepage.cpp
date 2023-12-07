3c3662604cd49cb161e2cfe7e59556bfd70523ff
DynoAbd786 <Abdua786@outlook.com>
2023-12-07 11:48:02 +0000

    completed the setup of 1 post. slider widget added and working, like buttion working (visually)

--- VideoPlayer/homepage.cpp
+++ VideoPlayer/homepage.cpp
@@ -2,14 +2,20 @@
 #include <QDir>
 #include <QDirIterator>
 #include <QUrl>
+#include <QVideoWidget>
 
 #include "homepage.h"
 #include "ui_homepage.h"
 #include "core/application.h"
 #include "mainwindow.h"
 
+// This is a variable used to indicate how many videos and users there are meant to be
+// THIS IS NOT TO INDICATE THE FINAL DESIGN. DEMONSTRATION PURPOSES ONLY
+#define SET_POSTS 4
 
 using namespace std;
+
+
 
 /**
  * @author Mustafa Yozgyur
@@ -28,7 +34,7 @@ HomePage::HomePage(QWidget *parent, MainWindow* main_window)
     // get the user by calling:
 //    User *user = Application::instance()->GetCore()->GetUser();
 
-    // begin to set up posts
+    // Begin to set up posts
 //    SetupPostsOnSuccessfulLogin();
 
     // u can use ui->scrollArea to get the ScrollArea btw.
@@ -59,20 +65,52 @@ void HomePage::ProfileButtonClicked()
 
 void HomePage::SetupPostsOnSuccessfulLogin()
 {
+    // Retrieve user class
+    User *p_User = Application::instance()->GetCore()->GetUser();
+
+    // Retrieve videos
     VideosFromDirectory = RetrieveVideosFromDirectory();
 
-    // TODO: retrieve videos
-//    vector<string> VideosLocation = RetrieveVideosFromDirectory();
+    // Set up layout to store videos to
+
+    // Set up a label to indicate no videos to show
+    QLabel *NoVideosToDisplayLabel;
 
     // TODO: loop through each video and set up each videos layout. store it to a list of layouts
+
+    // Check statements if:
+    // The user has no friends
+    if (VideosFromDirectory.empty() && p_User->GetFriends().empty())
+    {
+        NoVideosToDisplayLabel = new QLabel("<h3>You have not added any friends yet. Add some friends <h3>");
+    }
+    // The users friends has no videos to display
+    else if (VideosFromDirectory.empty())
+    {
+        NoVideosToDisplayLabel = new QLabel("<h3>Your friends have not uploaded their StaySimple videos. Check back Later</h3>");
+    }
+    // There are videos and friends to display
+    else
+    {
+        cout << "here" << endl;
+        PostWidgets = SetupPostWidgetsAndLayout(VideosFromDirectory);
+        ui->scrollAreaForPosts->setWidget(PostWidgets[0]);
+//        ui->scrollAreaForPosts->show();
+
+    }
+
+//    update();
+
+
         // need post metadata
 
     // TODO: setup the layouts in date order
 }
 
-vector<HomePage::Video> HomePage::RetrieveVideosFromDirectory()
+vector<HomePage::Video*> HomePage::RetrieveVideosFromDirectory()
 {
     // TODO (maybe): implement the situation where the path was entered by the user
+    // this may need to be taken care of in the login page
 
     // find path to videos
     QString AppDirPath = Application::instance()->applicationDirPath();
@@ -93,12 +131,15 @@ vector<HomePage::Video> HomePage::RetrieveVideosFromDirectory()
         AppDir.cdUp();
     }
 
+    // Enter the videos folder inside the assets folder
     AppDir.cd("videos");
+
+    // TEST LINE
     cout << AppDir.absolutePath().toStdString() << endl;
 
     // Find all videos in the videos directory and store it to a vector to return
     // MAY NEED TO MAKE A SUBCLASS FOR STORING VIDEO
-    vector<HomePage::Video> DataForVideos;
+    vector<HomePage::Video*> DataForVideos;
 
     // We are going to look through the directory for any videos
     // This has been adapted from "the.pro" and its code
@@ -126,12 +167,152 @@ vector<HomePage::Video> HomePage::RetrieveVideosFromDirectory()
             QDateTime CreationDateTime = FileInfo.birthTime();
 
             // Save video URL and DateTime to list of Videos
-            DataForVideos.push_back(HomePage::Video(Url, CreationDateTime));
+            DataForVideos.push_back(new HomePage::Video(Url, CreationDateTime));
+            cout << DataForVideos[0]->GetCreationTimeToString().toStdString() << endl;
         }
     }
 
     return DataForVideos;
 }
+
+vector<PostWidget*> HomePage::SetupPostWidgetsAndLayout(vector<HomePage::Video*> DataForVideos)
+{
+    // Set Posts list
+    vector<PostWidget*> Posts;
+    PostWidget *Post;
+
+    // Get user information and friends
+    User *p_User = Application::instance()->GetCore()->GetUser();
+    vector<string> UserFriends = p_User->GetFriends();
+
+    // Make 1 Post from the user and videos information
+    for (int NumPosts = 0; NumPosts < SET_POSTS; NumPosts++)
+    {
+        Posts.push_back(new PostWidget(UserFriends[NumPosts], DataForVideos[NumPosts]));
+    }
+
+    return Posts;
+}
+
+
+//SOME FUNCTION HERE??????
+//order by time function, asc order
+
+
+// Constructor for the PostWidget class
+PostWidget::PostWidget(string name, HomePage::Video *videoData)
+{
+    // Create widgets
+    CreateWidgets(name, videoData);
+
+    // Arrange widgets
+    ArrangeWidgets();
+
+    // Make connections
+    MakeConnections();
+}
+
+void PostWidget::CreateWidgets(string name, HomePage::Video *videoData)
+{
+    // Set up labels
+    m_NameLabel = new QLabel(QString::fromStdString(name));
+    m_TimeLabel = new QLabel(videoData->GetCreationTimeToString());
+
+    // Set up media player and video
+//    QPushButton *m_ClickablePlayerForPlayPause = new QPushButton();           // EXPERIMENTAL, CONSIDERING HAVING TO CLICK ON THE VIDEO TO PAUSE/PLAY IT, SIMILAR TO YOUTUBE SHORTS KINDA
+    m_MediaPlayer = new QMediaPlayer;
+    m_Video = new QVideoWidget(this);
+    m_MediaPlayer->setMedia(*videoData->GetVideoUrl());
+    m_MediaPlayer->setVideoOutput(m_Video);
+//    m_Video->setAspectRatioMode(Qt::IgnoreAspectRatio);
+
+    qint64 duration = m_MediaPlayer->duration();
+    m_MediaPlayer->play();
+
+    // Set up scroll bar for the video player
+    m_SliderForVideoSeeking = new QSlider(Qt::Horizontal, this);
+    m_SliderForVideoSeeking->setRange(0, duration);
+
+    // Set up like and comments button
+    m_LikeButton = new QPushButton();
+    m_LikeButton->setIcon(QIcon(":/assets/button_images/likeButtonOff.jpg"));
+    m_LikeButton->setCheckable(false);
+    m_CommentsButton = new QPushButton();
+    m_CommentsButton->setIcon(QIcon(":/assets/button_images/commentsButton.jpg"));
+}
+
+void PostWidget::ArrangeWidgets()
+{
+    // Arrange friend information and time taken to the top of the post
+    m_UserInformationLayout = new QHBoxLayout();
+    m_UserInformationLayout->addWidget(m_NameLabel);
+    m_UserInformationLayout->addStretch(1);
+    m_UserInformationLayout->addWidget(m_TimeLabel);
+
+    // Arrange video player and slider to the middle of the post
+    m_VideoPlayerLayout = new QVBoxLayout();
+    m_VideoPlayerLayout->addWidget(m_Video);
+    m_VideoPlayerLayout->addWidget(m_SliderForVideoSeeking);
+
+    // Arrange like and comment buttons to the bottom of the post
+    m_LikeAndCommentLayout = new QHBoxLayout();
+    m_LikeAndCommentLayout->addWidget(m_LikeButton);
+    m_LikeAndCommentLayout->addWidget(m_CommentsButton);
+
+    m_MainLayout = new QVBoxLayout();
+    m_MainLayout->addLayout(m_UserInformationLayout);
+    m_MainLayout->addLayout(m_VideoPlayerLayout);
+    m_MainLayout->addLayout(m_LikeAndCommentLayout);
+
+    setLayout(m_MainLayout);
+}
+
+void PostWidget::MakeConnections()
+{
+    // Connect slider to media player and vice versa
+    connect(m_SliderForVideoSeeking, SIGNAL(valueChanged(int)), this, SLOT(SetPosition(int)));
+    connect(m_MediaPlayer, SIGNAL(durationChanged(qint64)), this, SLOT(UpdateDuration(qint64)));
+    connect(m_MediaPlayer, SIGNAL(positionChanged(qint64)), this, SLOT(UpdatePosition(qint64)));
+
+    // Connect like button
+    connect(m_LikeButton, SIGNAL(clicked()), this, SLOT(ChangeLikeButtonStatus()));
+}
+
+void PostWidget::SetPosition(int position)
+{
+    m_MediaPlayer->setPosition(static_cast<qint64> (position));
+    m_MediaPlayer->play();
+}
+
+void PostWidget::UpdateDuration(qint64 duration)
+{
+    m_SliderForVideoSeeking->setMaximum(static_cast<int>(duration));
+}
+
+void PostWidget::UpdatePosition(qint64 position)
+{
+    m_SliderForVideoSeeking->setValue(static_cast<int>(position));
+}
+
+void PostWidget::ChangeLikeButtonStatus()
+{
+    if (!m_LikeButton->isChecked())
+    {
+        m_LikeButton->setIcon(QIcon(":/assets/button_images/likeButtonOn.jpg"));
+        m_LikeButton->setCheckable(true);
+//        update();
+    }
+    else
+    {
+        cout << "herrr2" << endl;
+        m_LikeButton->setIcon(QIcon(":/assets/button_images/likeButtonOff.jpg"));
+        m_LikeButton->setCheckable(false);
+        update();
+    }
+}
+
+
+//void HomePage::PostWidget::
 
 //// read in videos and thumbnails to this directory
 //std::vector<TheButtonInfo> getInfoIn (std::string loc) {
--- VideoPlayer/homepage.h
+++ VideoPlayer/homepage.h
@@ -8,6 +8,8 @@
 #include <QMediaPlayer>
 #include <QPushButton>
 #include <QDateTime>
+#include <QVBoxLayout>
+#include <QSlider>
 
 
 //#include "core/user.h"
@@ -21,6 +23,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class HomePage; }
 QT_END_NAMESPACE
 
+
+class PostWidget;
 class HomePage : public QWidget
 {
     Q_OBJECT
@@ -28,12 +32,7 @@ public:
     explicit HomePage(QWidget *parent = nullptr, MainWindow* main_window = nullptr);
     ~HomePage();
 
-public slots:
-    void ProfileButtonClicked();
-    void SetupPostsOnSuccessfulLogin();
 
-
-private:
     // @MUSTAFA, THIS IS A SIMPLE IMPLEMENTATION OF A VIDEO CLASS. IF YOU WANT TO PULL THIS TO USE INSIDE THE VIDEO_DB, YOU MIGHT BE ABLE TO USE IT
     // i dont know what you have planned for that class, nor i know how to actually implement it the way you are thinking
     // THE ENTIRE CLASS HAS BEEN INLINED TO THE POST_WIDGET CLASS. THERE IS NO .CPP CLASS FILE FOR THIS
@@ -47,8 +46,11 @@ private:
         QUrl *GetVideoUrl() { return m_VideoUrl; }
         QTime GetCreationTime() { return m_CreationDateTime.time(); }
         QDate GetCreationDate() { return m_CreationDateTime.date(); }
+        QString GetCreationTimeToString() { return m_CreationDateTime.time().toString(); }
+        QString GetCreationDateToString() { return m_CreationDateTime.date().toString(); }
 
         // Setter methods
+        // THESE MIGHT BE REDUNDANT
         void SetVideoUrl(QUrl *Url) { m_VideoUrl = Url; }
         void SetVideoDateTime(QDateTime DateTime) { m_CreationDateTime = DateTime; }
 
@@ -58,28 +60,75 @@ private:
         QDateTime m_CreationDateTime;
     };
 
-    class PostWidget: public QWidget
-    {
-    public:
-        PostWidget(std::string name, std::string timePosted, std::string videoUrl);
+    // Post creation functions
+    vector<PostWidget*> SetupPostWidgetsAndLayout(vector<HomePage::Video*> DataForVideos);
+
+public slots:
+    void ProfileButtonClicked();
+    void SetupPostsOnSuccessfulLogin();
 
 
-    private:
-        QLabel m_NameLabel;
-        Video m_Video;
-        QPushButton m_LikeButton;
-        QPushButton m_CommentsButton;
-    };
+
+protected:
+    vector<Video*> RetrieveVideosFromDirectory();
 
 
+
+
+
+private:
     MainWindow* p_MainWindow;
     Ui::HomePage* ui;
-    vector<Video> VideosFromDirectory;
+    vector<Video*> VideosFromDirectory;
+    vector<PostWidget*> PostWidgets;
+    QVBoxLayout LayoutForAllPosts;
 
     // Functions used by the SetupPostsOnSuccessfulLogin() function call
-    vector<Video> RetrieveVideosFromDirectory();
+//    vector<PostWidget> SetupPostWidgetsAndLayout(vector<Video> DataOfVideos);
 
 
+
+};
+
+
+class PostWidget: public QWidget
+{
+    Q_OBJECT
+public:
+    // Constructor for PostWidget
+    PostWidget(std::string name, HomePage::Video *videoData);
+
+public slots:
+    // Post slots
+    void UpdateDuration(qint64 duration);
+    void UpdatePosition(qint64 position);
+    void SetPosition(int position);
+    void ChangeLikeButtonStatus();
+
+private:
+    // Functions to set up post class
+    void CreateWidgets(string name, HomePage::Video *videoData);
+    void ArrangeWidgets();
+    void MakeConnections();
+
+
+
+    // Widgets
+    QLabel *m_NameLabel;
+    QLabel *m_TimeLabel;
+    QPushButton *m_LikeButton;
+    QPushButton *m_CommentsButton;
+    QSlider *m_SliderForVideoSeeking;
+    QVideoWidget *m_Video;
+    // And the respective video player
+    QMediaPlayer *m_MediaPlayer;
+
+    // Layouts to store the widgets and layouts to
+    QHBoxLayout *m_UserInformationLayout;
+    QVBoxLayout *m_VideoPlayerLayout;
+    QHBoxLayout *m_LikeAndCommentLayout;
+
+    QVBoxLayout *m_MainLayout;
 
 };
 
